a31d8ca6bfdb17fa1eb1ef9a0d31d0a7332b3533
Sumit Kumar Jha <42186301+sumitkjha@users.noreply.github.com>
2026-03-23 00:38:27 +0530

    Update Footer.tsx
    update black and white nomadic plan logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import { Mail as MailIcon, Phone as PhoneIcon, Facebook, Instagram, Youtube } fr
 import { Link } from "react-router-dom";
 import WhatsAppIcon from "./icons/WhatsAppIcon";
 import { Button } from "./ui/button";
+import  logoImage  from "@/assets/nomadicplan_logo_bw.png"; 
 
 const Footer = () => {
   return (
@@ -13,7 +14,7 @@ const Footer = () => {
             {/* Brand */}
             <div className="lg:col-span-1">
               <div className="flex items-center space-x-2 mb-6">
-                <img src="https://cdn.builder.io/api/v1/image/assets%2Fde743b16560c4ea5a4a46e65a2543876%2F4be0568d99d2469baa7ef6c274a8a1b2?format=webp&width=800" alt="Nomadic Plan logo" className="h-12 w-auto" />
+                <img src={logoImage} alt="NomadicPlan_logo" className="h-12 w-auto sm:h-10" />
                 <span className="text-2xl font-bold text-white">Nomadic<span className="text-secondary">Plan</span></span>
               </div>
               <p className="text-white/80 mb-6 leading-relaxed">
